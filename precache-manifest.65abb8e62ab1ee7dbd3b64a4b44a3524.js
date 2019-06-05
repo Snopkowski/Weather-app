c06c2abdd39ed6273671a6b5d7a9d653f18686ca
Snopkowski <wojciechsnopkowski@gmail.com>
2019-06-05 11:30:17 +0100

    Updates

---
+++ precache-manifest.65abb8e62ab1ee7dbd3b64a4b44a3524.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c4ae10f7166770f43d9bc1a25990cbd",
+    "url": "/Weather-app/index.html"
+  },
+  {
+    "revision": "81728dddffd525cdd76b",
+    "url": "/Weather-app/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3ba9fce0edc80ba533e5",
+    "url": "/Weather-app/static/css/main.6fba4841.chunk.css"
+  },
+  {
+    "revision": "81728dddffd525cdd76b",
+    "url": "/Weather-app/static/js/2.ba7a277f.chunk.js"
+  },
+  {
+    "revision": "3ba9fce0edc80ba533e5",
+    "url": "/Weather-app/static/js/main.5933c943.chunk.js"
+  },
+  {
+    "revision": "cd23450c59ffd8f53315",
+    "url": "/Weather-app/static/js/runtime~main.9b6b42b8.js"
+  }
+]);
